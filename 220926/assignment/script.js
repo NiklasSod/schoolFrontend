32a3ad0d365f13cc4f99b34d5821aa11a14824f5
NiklasSod <nma.prob@gmail.com>
2022-09-26 17:27:41 +0200

    assignment 2 of 3 done

--- 220926/assignment/script.js
+++ 220926/assignment/script.js
@@ -1,6 +1,8 @@
 let refreshBtn = document.getElementById("refreshBtn");
 let oldParagraph = document.getElementById("oldParagraph");
 let updateParagraphBtn = document.getElementById("updateParagraphBtn");
+let changeLinkBtn = document.getElementById("changeLinkBtn");
+let link = document.getElementById("link");
 
 refreshBtn.addEventListener("click", () => {
   location.reload();
@@ -12,3 +14,9 @@ updateParagraphBtn.addEventListener("click", () => {
   updateParagraphBtn.style.display = "none";
 });
 
+changeLinkBtn.addEventListener("click", () => {
+  link.href = "https://google.com";
+  link.innerText = "https://google.com";
+  changeLinkBtn.innerText = "Länken ändrad!";
+  changeLinkBtn.disabled = true;
+});
